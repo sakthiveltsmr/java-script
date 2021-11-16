73109e545ae0d960edc00c5448a3d5dc1393330d
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-16 13:00:56 +0530

    Add files via upload

---
+++ js-Function/index.js
@@ -0,0 +1,13 @@
+function user() {
+  let name = "sakthi";
+  let age = 24;
+  let msg = "hello " + name + " ur age is " + age;
+  document.write(msg);
+} // without parameter
+user();
+
+function employe(name) {
+  let Rollno = 161700;
+  document.write(name + " " + Rollno);
+} // withparameter
+employe("sakthi");
