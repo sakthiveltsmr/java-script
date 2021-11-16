e497ab41dd1957b9385f1798773f092b099becec
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-16 12:59:45 +0530

    Add files via upload

---
+++ js-datatypes/index.js
@@ -0,0 +1,8 @@
+let name = "sakthi"; // String
+let age = 24; // Integer / number
+let isyoung = true; // Boolean
+let lastname = undefined; // undefined
+let trophy = null; // null
+document.write(
+  name + " " + age + " " + isyoung + " " + lastname + " " + trophy
+);
