c3f5d8e820a0bc37ed299b32f063792f5fc4c91e
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-19 11:53:51 +0530

    Add files via upload

---
+++ js-operator/index.js
@@ -0,0 +1,33 @@
+let num1 = 10;
+let num2 = 20;
+// arithmatic operator//
+document.write(num1 + num2);
+document.write(num1 - num2);
+document.write(num1 * num2);
+document.write(num1 / num2);
+document.write(num1 % num2);
+document.write(num1 ** num2);
+
+//increment(++)
+document.write(++num1); //pre increment
+document.write(num1);
+
+document.write(num2++); // post increment
+document.write(num2);
+
+//decrement(--)
+document.write(--num1); //pre decrement
+document.write(num1);
+
+document.write(num2--); // post decrement
+document.write(num2);
+
+//assignment operators
+let x = 10;
+x += 2;
+document.write(x);
+
+//comparision operators
+let y = 5;
+document.write(y <= 10);
+document.write(y > 5);
