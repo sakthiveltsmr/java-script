c9c4de05bcd25ddcbf99a9387a2d3b6adff7aedf
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-19 11:54:29 +0530

    Add files via upload

---
+++ js-ternory operators/index.js
@@ -0,0 +1,9 @@
+//Ternory operators
+
+let age = 24;
+let type = age > 18 ? "adult tickets" : "child ticket";
+document.write(type);
+
+let marks = prompt("enter ur marks");
+let result = marks > 35 ? "pass" : "fail";
+document.writeln("your result is " + result);
