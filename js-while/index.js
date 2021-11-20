132f56efb6e853256cd1a827984a8f943eaf164b
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-20 16:06:29 +0530

    Add files via upload

---
+++ js-while/index.js
@@ -0,0 +1,15 @@
+//while
+let i = 1;
+while (i <= 10) {
+  if (i % 2 !== 0) {
+    console.log("odd number is ", i);
+  }
+  i++;
+}
+// do while
+do {
+  if (i % 2 !== 0) {
+    console.log("do while---odd number is ", i);
+  }
+  i++;
+} while (i <= 10);
