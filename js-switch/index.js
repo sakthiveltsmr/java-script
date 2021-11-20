339c237d9cfee340d5b5806c76232367bca7b396
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-20 16:05:03 +0530

    Add files via upload

---
+++ js-switch/index.js
@@ -0,0 +1,34 @@
+/*let mark = prompt("enter the marks");
+switch (true) {
+  case mark >= 35:
+    console.log("pass");
+    break;
+  case mark >= 95:
+    console.log("first class pass");
+    break;
+  default:
+    console.log("fail");
+}*/
+
+switch (new Date().getDay()) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+}
