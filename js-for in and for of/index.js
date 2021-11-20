9c0c2af7998b37885ba13fa6ec33c5191db63a05
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-20 16:02:16 +0530

    Add files via upload

---
+++ js-for in and for of/index.js
@@ -0,0 +1,15 @@
+//for in
+let person = {
+  name: "sakthi",
+  age: 24,
+  sex: "male",
+};
+for (let employe in person) {
+  console.log(employe + ":" + person[employe]);
+}
+
+//for of
+let colors = ["red,green,blue"];
+for (let sakthi of colors) {
+  console.log("colors :" + sakthi);
+}
