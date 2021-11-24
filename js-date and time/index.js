df57ae9fe509d85e577c615ca9d3dc0e2628f951
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-24 14:16:19 +0530

    Add files via upload

---
+++ js-date and time/index.js
@@ -0,0 +1,8 @@
+let date = new Date();
+console.log(date);
+console.log("date is", date.getDate());
+console.log("day is ", date.getDay());
+console.log("hour is", date.getHours());
+console.log("minutes is ", date.getMinutes());
+console.log("year is", date.getUTCFullYear());
+console.log("month is", date.getMonth());
