8fd9cb2cafbfb69cde60c56fe859acfa4bbbe387
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-24 14:15:52 +0530

    Add files via upload

---
+++ js-math function/index.js
@@ -0,0 +1,10 @@
+console.log(Math.PI);
+let a = 5;
+let b = 6.5;
+let c = -50;
+console.log(Math.sqrt(10));
+console.log(Math.round(b));
+console.log(Math.log(1));
+console.log(Math.log(10));
+console.log(Math.abs(c));
+console.log(Math.ceil(c));
