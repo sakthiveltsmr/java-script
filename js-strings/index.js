347d656fb01c498c27176e1086c68ff67acd87e8
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2021-11-24 14:19:13 +0530

    Add files via upload

---
+++ js-strings/index.js
@@ -0,0 +1,18 @@
+//primitive string values
+
+let firstname = "Sakthi";
+let Middelname = "vel";
+let lastname = `Ramasamy`;
+
+console.log(firstname.charAt(3));
+console.log(firstname.concat(" ", lastname));
+console.log(lastname.toLowerCase());
+console.log(lastname.toUpperCase());
+console.log(firstname.repeat(2));
+console.log(firstname.replace("Sakthi", "Ram"));
+console.log(firstname.slice(2));
+let para = "hai hello";
+console.log(para.slice(" ", 3));
+console.log(para.substr(4));
+let msg = "This is sakthi! ";
+console.log(msg.trim());
